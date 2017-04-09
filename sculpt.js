bef2484cd7fb6500cdc9a674ca57a4d520f9c2f7
agorischek sterling <alex.gorischek@gmail.com>
2017-04-08 18:46:51 -0700

    Rename function to "element"
    And change the property previously called “element” to “tag”.  In
    practice, “element()” appears more readable.

--- sculpt.js
+++ sculpt.js
@@ -1,17 +1,17 @@
-function sculpt(object){
-    var element = object.element;
+function element(object){
+    var tag = object.tag;
     var content = object.content;
     var attributes = "";
     var html = "";
     for (var property in object){
         if (object.hasOwnProperty(property)) {
-            if(property != "element" && property != "content"){
+            if(property != "tag" && property != "content"){
                 attributes = attributes.concat(" " + property + "=\"" + object[property] + "\"");
             }
         }
     }
-    var openingTag = "<" + element + " " + attributes + ">";
-    var closingTag = "</" + element + ">";
+    var openingTag = "<" + tag + " " + attributes + ">";
+    var closingTag = "</" + tag + ">";
     if(Array.isArray(content)){
         for (var index = 0; index < content.length; ++index) {
             html = html.concat(openingTag + content[index] + closingTag);
