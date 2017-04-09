ad8db5569d84fa6389030aeb544c7b99403fb03d
agorischek sterling <alex.gorischek@gmail.com>
2017-04-08 18:42:41 -0700

    Add support for arrays of content

--- sculpt.js
+++ sculpt.js
@@ -2,6 +2,7 @@ function sculpt(object){
     var element = object.element;
     var content = object.content;
     var attributes = "";
+    var html = "";
     for (var property in object){
         if (object.hasOwnProperty(property)) {
             if(property != "element" && property != "content"){
@@ -9,6 +10,15 @@ function sculpt(object){
             }
         }
     }
-    var html = "<" + element + " " + attributes + ">" + content + "</" + element + ">";
+    var openingTag = "<" + element + " " + attributes + ">";
+    var closingTag = "</" + element + ">";
+    if(Array.isArray(content)){
+        for (var index = 0; index < content.length; ++index) {
+            html = html.concat(openingTag + content[index] + closingTag);
+        }
+    }
+    else{
+        html = openingTag + content + closingTag;
+    }
     return html;
 }
