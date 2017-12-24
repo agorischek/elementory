8f46bedda274dae0e56456f2b56a466b19b635df
agorischek <alex.gorischek@gmail.com>
2017-12-24 11:07:05 -0600

    Address minor formatting inconsistencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var rename = require("gulp-rename")
+var gulp = require("gulp");
+var uglify = require("gulp-uglify");
+var rename = require("gulp-rename");
 
-gulp.task('build', function(){
-  return gulp.src('elementory.js')
+gulp.task("build", function(){
+  return gulp.src("elementory.js")
     .pipe(uglify())
     .pipe(rename("elementory.min.js"))
-    .pipe(gulp.dest('./'))
+    .pipe(gulp.dest("./"))
 });
 
-gulp.task('default', [ 'build' ]);
+gulp.task("default", ["build"]);
